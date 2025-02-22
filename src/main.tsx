cdd62277adf6518895c0d1c85e3bb771a9e2664a
Alexander Baquero <brayanalexanderb@gmail.com>
2025-02-22 16:15:26 -0500

    Add react-icons and update component structure
    - Install react-icons library
    - Refactor main routing to use new Home component
    - Update import paths for Layout and Home components

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import Layout from "./Layout.tsx";
-import App from "./App.tsx";
+import Layout from "./Components/Templates/Layout.tsx";
+import Home from "./Components/Pages/Home.tsx";
 
 import "./index.css";
 
@@ -10,8 +10,8 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout children={<App />} />}>
-          <Route index element={<App />} />
+        <Route element={<Layout children={<Home />} />}>
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </BrowserRouter>
