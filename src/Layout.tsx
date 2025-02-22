02e2f71cdf27e9f0acdab7888f95cd4dc5b28b42
Alexander Baquero <brayanalexanderb@gmail.com>
2025-02-22 13:57:47 -0500

    Add Layout component and configure routing with React Router

---
+++ src/Layout.tsx
@@ -0,0 +1,16 @@
+import NavBar from "./Components/Molecules/NavBar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>{children}</main>
+      {/* <Footer /> */}
+      <footer className="debug">Footer</footer>
+    </>
+  );
+};
+
+export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./Layout.tsx";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout children={<App />} />}>
+          <Route index element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
